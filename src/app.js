9601cf012b201c208a530833c7aa91770bf1a7e1
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-11-18 02:15:29 +0900

    Feat: router 기능 추가
    article의 a태그영역 event target 수정 필요
    article 상세화면 url 변경 필요

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import base from "./views/base.js";
+import tech from "./views/tech.js";
+import article from "./views/article.js";
+import { page404 } from "./views/404.js";
+
+const app = document.querySelector("#app");
+app.innerHTML = base;
+
+const routes = [
+  { path: "/", view: tech },
+  { path: "/article", view: tech },
+  { path: "/article/:id", view: article },
+];
+
+const router = async () => {
+  const pathMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: window.location.pathname === route.path,
+    };
+  });
+  console.log(pathMatches);
+
+  let match = pathMatches.find((pathMatch) => pathMatch.isMatch);
+  console.log(match);
+
+  const contents = document.querySelector("#contents");
+
+  if (!match) {
+    console.log("error");
+    contents.innerHTML = page404;
+    return;
+  } else {
+    const view = new match.route.view();
+    contents.innerHTML = await view.getHtml();
+  }
+};
+
+const navigateTo = (url) => {
+  console.log(url);
+  window.history.pushState(null, null, url);
+  router();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    const target = e.target;
+    console.log(e, e.target);
+    // finding only anchor tag
+    if (!(target instanceof HTMLAnchorElement)) return;
+
+    // preventing page refresh
+    e.preventDefault();
+    navigateTo(target.href);
+  });
+  router();
+});
+
+window.addEventListener("popstate", router);
