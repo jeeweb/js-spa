f9177fe1a1ceb53be69374967098dd926849a606
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-11-24 23:37:54 +0900

    Refactor: router, getArticles 함수 분리

--- src/router.js
+++ src/router.js
@@ -13,14 +13,33 @@ const pathToRegex = (path) =>
   new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
 const fetchData = () => {
-  return fetch("/src/db.json")
-    .then((res) => res.json())
-    .then((data) => data);
+  return fetch("/src/db.json").then((res) => res.json());
+};
+
+const getArticles = (matchPath) => {
+  const view = new matchPath.route.view();
+  const id = matchPath.result[1];
+
+  fetchData().then((data) => {
+    const dataType = !id
+      ? matchPath.route.view.name.toLowerCase()
+      : data.find((item) => item.id === id).type;
+
+    const dataGroupByType = data.filter((item) => item.type === dataType);
+
+    if (!id) {
+      view.getData(dataGroupByType);
+    } else {
+      const article = data.find((item) => item.id === id);
+      view.getData(article);
+    }
+  });
 };
 
 const router = () => {
   const { pathname } = window.location;
   const contents = document.querySelector("#contents");
+
   const pathMatch = routes.map((route) => {
     return {
       route: route,
@@ -34,26 +53,7 @@ const router = () => {
     contents.innerHTML = page404;
     return;
   } else {
-    const view = new match.route.view();
-    const id = match.result[1];
-
-    const getArticles = () => {
-      fetchData().then((data) => {
-        const dataType = !id
-          ? match.route.view.name.toLowerCase()
-          : data.find((item) => item.id === id).type;
-
-        const dataGroupByType = data.filter((item) => item.type === dataType);
-
-        if (!id) {
-          view.getData(dataGroupByType);
-        } else {
-          const article = data.find((item) => item.id === id);
-          view.getData(article);
-        }
-      });
-    };
-    getArticles();
+    getArticles(match);
   }
 };
 
