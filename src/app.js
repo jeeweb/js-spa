ce9e1b5c8291707c0991ce8bc52c76b4fbbfda56
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-11-24 23:42:06 +0900

    Refactor: 현재 도메인과 다른 url 이동 가능하게 수정 (채용 바로가기)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import base from "./views/base.js";
 import router from "./router.js";
 
+const currentDomain = location.origin;
 const app = document.querySelector("#app");
 app.innerHTML = base;
 
 const navigateTo = (url) => {
+  if (url !== currentDomain) {
+    location.href = url;
+  }
   window.history.pushState(null, null, url);
   router();
 };
