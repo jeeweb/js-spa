218ec6be47f240dff18ba3670a5673b43ad85041
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-11-30 23:36:45 +0900

    Refactor: render함수 이동 및 코드 정리

--- src/controller.js
+++ src/controller.js
@@ -1,28 +1,41 @@
 import Model from "./model.js";
+import { page404 } from "./views/404.js";
 
 export default class Controller {
-  constructor(view) {
-    this.view = new view();
+  constructor() {
     this.model = new Model();
   }
-  getArticles(matchPath) {
-    const id = matchPath.result[1];
 
-    const fetchData = async () => {
-      await this.model.then((data) => {
-        const dataType = !id
-          ? matchPath.route.view.name.toLowerCase()
-          : data.find((item) => item.id === id).type;
-        const dataGroupByType = data.filter((item) => item.type === dataType);
-        //console.log(dataType, dataGroupByType);
-        if (!id) {
-          this.view.getData(dataGroupByType);
-        } else {
-          const article = data.find((item) => item.id === id);
-          this.view.getData(article);
-        }
-      });
-    };
-    fetchData();
+  getView(view) {
+    return new view();
+  }
+
+  async getArticles(path) {
+    const id = path.result[1];
+    const matchView = path.route.view;
+
+    await this.model.then((data) => {
+      const dataType = !id
+        ? matchView.name.toLowerCase()
+        : data.find((item) => item.id === id).type;
+      const dataGroupByType = data.filter((item) => item.type === dataType);
+
+      if (!id) {
+        this.getView(matchView).getData(dataGroupByType);
+      } else {
+        const article = data.find((item) => item.id === id);
+        this.getView(matchView).getData(article);
+      }
+    });
+  }
+
+  render(path) {
+    const contents = document.querySelector("#contents");
+    if (!path) {
+      contents.innerHTML = page404;
+      return;
+    } else {
+      this.getArticles(path);
+    }
   }
 }
--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Controller from "./controller.js";
 import tech from "./views/tech.js";
 import article from "./views/article.js";
 import design from "./views/design.js";
-import { page404 } from "./views/404.js";
 
 const routes = [
   { path: "/", view: tech },
@@ -15,24 +14,15 @@ const pathToRegex = (path) =>
 
 const router = () => {
   const { pathname } = window.location;
-  const contents = document.querySelector("#contents");
-
   const pathMatch = routes.map((route) => {
     return {
       route: route,
       result: pathname.match(pathToRegex(route.path)),
     };
   });
-
   const match = pathMatch.find((el) => el.result !== null);
-
-  if (!match) {
-    contents.innerHTML = page404;
-    return;
-  } else {
-    const controller = new Controller(match.route.view);
-    controller.getArticles(match);
-  }
+  const controller = new Controller();
+  controller.render(match);
 };
 
 export default router;
