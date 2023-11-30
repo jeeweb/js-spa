aeb94738c705e3a6c405a08077be551d6a0668de
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-11-30 23:40:14 +0900

    Feat: 화면 이동 시 상단으로 이동

--- src/router.js
+++ src/router.js
@@ -23,6 +23,7 @@ const router = () => {
   const match = pathMatch.find((el) => el.result !== null);
   const controller = new Controller();
   controller.render(match);
+  window.scrollTo(0, 0); // 화면 이동시 상단으로 이동
 };
 
 export default router;
